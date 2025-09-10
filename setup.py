06115cc795fd20af0f2743d2bd66779495579804
Jeff Turner <jeff@torusoft.com>
2025-09-10 09:18:25 -0300

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="django-admin-query-executor",
-    version="1.0.0",
+    version="1.1.0",
     author="Jeff Turner",
     author_email="jeff@torusoft.com",
     description="Execute Django ORM queries directly from the admin interface",
